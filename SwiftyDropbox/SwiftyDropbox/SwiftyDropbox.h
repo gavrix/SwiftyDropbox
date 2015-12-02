b386c7780f582858584de65652a8d07f01ee843f
Sergey Gavrilyuk <sergey.gavrilyuk@gmail.com>
2015-12-01 21:54:09 -0500

    Project added

---
+++ SwiftyDropbox/SwiftyDropbox/SwiftyDropbox.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyDropbox.h
+//  SwiftyDropbox
+//
+//  Created by Sergii Gavryliuk on 2015-12-01.
+//  Copyright © 2015 Dropbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDropbox.
+FOUNDATION_EXPORT double SwiftyDropboxVersionNumber;
+
+//! Project version string for SwiftyDropbox.
+FOUNDATION_EXPORT const unsigned char SwiftyDropboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDropbox/PublicHeader.h>
+
+
